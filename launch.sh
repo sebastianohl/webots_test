113dcb901f763340f4c0d3d05e259ec6abaf30e5
Sebastian Ohl <s.ohl@ostfalia.de>
2023-08-30 15:29:12 +0200

    working ros2 cloud setup

--- launch.sh
+++ launch.sh
@@ -1,9 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-export WEBOTS_PORT=`./find_port.py`
-xdg-open http://localhost?URL=ws://localhost:${WEBOTS_PORT}
-
-export DISPLAY=:1
 source install/local_setup.sh
 
 echo "start in ${WEBOTS_PORT}"
--- src/uav_launch/launch/robot_launch.py
+++ src/uav_launch/launch/robot_launch.py
@@ -19,33 +19,128 @@ import os
 import pathlib
 import launch
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions.path_join_substitution import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
-from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
-from webots_ros2_driver.utils import controller_url_prefix
+from launch.launch_context import LaunchContext
+from launch.substitution import Substitution
+from launch.substitutions import TextSubstitution
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
 
 
-def get_ros2_nodes(*args):
+class CloudWebotsController(ExecuteProcess):
+
+    def __init__(self,
+                 ip_address,
+                 port,
+                 output='screen',
+                 respawn=False,
+                 remappings=[],
+                 namespace='',
+                 parameters=[],
+                 robot_name='',
+                 **kwargs):
+        webots_controller = (os.path.join(
+            get_package_share_directory('webots_ros2_driver'), 'scripts',
+            'webots-controller'))
+
+        protocol = "tcp"
+
+        robot_name_option = [] if not robot_name else [
+            '--robot-name=' + robot_name
+        ]
+        ip_address_option = [] if not ip_address else [
+            '--ip-address=' + ip_address
+        ]
+
+        ros_arguments = []
+        for item in remappings:
+            key, value = item
+            remap = f'{key}:={value}'
+            ros_arguments.append('-r')
+            ros_arguments.append(remap)
+        if (namespace):
+            remap = f'__ns:=/{namespace}'
+            ros_arguments.append('-r')
+            ros_arguments.append(remap)
+        for item in parameters:
+            if isinstance(item, dict):
+                for key, value in item.items():
+                    parameter = [
+                        f'{key}:=', value if isinstance(value, Substitution)
+                        else TextSubstitution(text=str(value))
+                    ]
+                    ros_arguments.append('-p')
+                    ros_arguments.append(parameter)
+        file_parameters = [
+            item for item in parameters if isinstance(item, str)
+        ]
+
+        ros_args = ['--ros-args'] if ros_arguments else []
+        params_file = ['--params-file'] if file_parameters else []
+
+        node_name = 'webots_controller' + (
+            ('_' + robot_name) if robot_name else '')
+        super().__init__(
+            output=output,
+            cmd=[
+                webots_controller,
+                *robot_name_option,
+                ['--protocol=', protocol],
+                *ip_address_option,
+                ['--port=', port],
+                'ros2',
+                *ros_args,
+                *ros_arguments,
+                *params_file,
+                *file_parameters,
+            ],
+            name=node_name,
+            respawn=respawn,
+            # Set WEBOTS_HOME to package directory to load correct controller library
+            additional_env={
+                'WEBOTS_HOME': get_package_prefix('webots_ros2_driver')
+            },
+            **kwargs)
+
+    def execute(self, context: LaunchContext):
+        return super().execute(context)
+
+    def _shutdown_process(self, context, *, send_sigint):
+        return super()._shutdown_process(context, send_sigint=send_sigint)
+
+
+def get_ros2_nodes(ip_address, port, *args):
     package_dir = get_package_share_directory('uav_launch')
-    robot_description = pathlib.Path(
-        os.path.join(package_dir, 'resource',
-                     'mavic_webots.urdf')).read_text()
+    #robot_description = str(
+    #    pathlib.Path(os.path.join(package_dir, 'resource',
+    #                              'mavic_webots.urdf')))
 
-    mavic_driver = Node(package='webots_ros2_driver',
-                        executable='driver',
-                        output='screen',
-                        additional_env={
-                            'WEBOTS_CONTROLLER_URL':
-                            controller_url_prefix() + 'Mavic_2_PRO'
-                        },
-                        parameters=[
-                            {
-                                'robot_description': robot_description
-                            },
-                        ])
+    #mavic_driver = Node(package='webots_ros2_driver',
+    #                    executable='driver',
+    #                    output='screen',
+    #                    additional_env={
+    #                        'WEBOTS_CONTROLLER_URL':
+    #                        controller_url_prefix + 'Mavic_2_PRO'
+    #                    },
+    #                    parameters=[
+    #                        {
+    #                            'robot_description': robot_description
+    #                        },
+    #                    ])
+    robot_description_path = os.path.join(package_dir, 'resource',
+                                          'mavic_webots.urdf')
+    mavic_driver = CloudWebotsController(robot_name='Mavic_2_PRO',
+                                         ip_address=ip_address,
+                                         port=port,
+                                         parameters=[
+                                             {
+                                                 'robot_description':
+                                                 robot_description_path
+                                             },
+                                         ],
+                                         respawn=True)
+
     controller = Node(
         package='uav_control',
         # package='uav_control_cpp',
@@ -74,8 +169,14 @@ def get_ros2_nodes(*args):
         fusion,
     ]
 
+
 class CloudRos2SupervisorLauncher(Node):
-    def __init__(self, controller_url_prefix, output='screen', respawn=True, **kwargs):
+
+    def __init__(self,
+                 controller_url_prefix,
+                 output='screen',
+                 respawn=True,
+                 **kwargs):
         # Launch the Ros2Supervisor node
         super().__init__(
             package='webots_ros2_driver',
@@ -83,17 +184,24 @@ class CloudRos2SupervisorLauncher(Node):
             output=output,
             # Set WEBOTS_HOME to the webots_ros2_driver installation folder
             # to load the correct libController libraries from the Python API
-            additional_env={'WEBOTS_CONTROLLER_URL': controller_url_prefix + 'Ros2Supervisor',
-                            'WEBOTS_HOME': get_package_prefix('webots_ros2_driver')},
+            additional_env={
+                'WEBOTS_CONTROLLER_URL':
+                controller_url_prefix + 'Ros2Supervisor',
+                'WEBOTS_HOME': get_package_prefix('webots_ros2_driver')
+            },
             respawn=respawn,
-            **kwargs
-        )
+            **kwargs)
+
 
 def generate_launch_description():
     package_dir = get_package_share_directory('uav_launch')
     world = LaunchConfiguration('world')
 
-    ros2_supervisor = CloudRos2SupervisorLauncher(controller_url_prefix="ws://172.17.0.1:2001")
+    ip_address = "141.41.39.177"
+    port = "2002"
+    print(f"tcp://{ip_address}:{port}")
+    ros2_supervisor = CloudRos2SupervisorLauncher(
+        controller_url_prefix=f"tcp://{ip_address}:{port}/")
 
     # The following line is important!
     # This event handler respawns the ROS 2 nodes on simulation reset (supervisor process ends).
@@ -108,4 +216,4 @@ def generate_launch_description():
 
         # Add the reset event handler
         reset_handler
-    ] + get_ros2_nodes())
+    ] + get_ros2_nodes(ip_address=ip_address, port=port))
